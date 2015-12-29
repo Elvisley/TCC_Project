6198429b08a3889b4688953bb9362b33e8ff07e6
Elvisley Souza <elvis.s.p90@gmail.com>
2015-12-29 12:46:02 -0200

    Adicionando testes unitarios

---
+++ ConsoleApplication8/TestesUnitarios/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication8.Domain;
+
+namespace TestesUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Register re = new Register();
+            re.nomecredor = "ELVISLEY SOUZA";
+
+            string teste = re.retirarCaracteresEspeciais(re.nomecredor);
+
+            Assert.AreEqual("ELVISLEYSOUZA", teste);
+        }
+    }
+}
